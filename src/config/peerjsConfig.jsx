fb53a5865b240614c24f4ec024b7adc36d7db3ec
ROHIT SADHU <rohitsadhupersonal@gmail.com>
2023-04-16 14:17:09 +0530

    -peerserver

--- src/config/peerjsConfig.jsx
+++ src/config/peerjsConfig.jsx
@@ -2,11 +2,7 @@ import Peer from "peerjs";
 import { v4 as uuidv4 } from 'uuid';
 
 // generate new peer instance
-const peer = new Peer(uuidv4(), {
-    host: 'localhost',
-    port: '1337',
-    path: 'meetclone'
-});
+const peer = new Peer(uuidv4());
 
 // make peer connection
 peer.on('open', (id) => {
